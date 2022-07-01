87ca81686b2bf495aef2cc9f6e2f443b4f9ea48b
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-01 21:36:46 +0400

    fix recommendation, change file structure, separete code...

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import { useState, useEffect, useRef } from "react";
+import React from "react";
 import "./App.scss";
-import HelloPhoto from "./resourse/hello.png";
+import Popup from "./components/Popup";
+
 
 function App() {
-  const ref = useRef();
-
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  useOnClickOutside(ref, () => setModalOpen(false));
   return (
-    <div className="body">
-      {isModalOpen ? (
-        <div className="modal__backdrop">
-          <div className="modal__container" ref={ref}>
-            👋 Hello my dear friend...
-            <img src={HelloPhoto} alt="this is hello" />
-            <button onClick={() => setModalOpen(false)}>Close</button>
-            <div className="buttonContainer"></div>
-          </div>
-        </div>
-      ) : (
-        <button onClick={() => setModalOpen(true)}>Open Modal</button>
-      )}
-    </div>
-  );
+    <Popup/>
+  )
 
-  function useOnClickOutside(ref, handler) {
-    useEffect(() => {
-      const listener = (event) => {
-        if (!ref.current || ref.current.contains(event.target)) {
-          return;
-        }
-        handler(event);
-      };
-      document.addEventListener("mousedown", listener);
-      document.addEventListener("touchstart", listener);
-      return () => {
-        document.removeEventListener("mousedown", listener);
-        document.removeEventListener("touchstart", listener);
-      };
-    }, [ref, handler]);
-  }
 }
-
 export default App;
---
+++ src/components/Popup.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import HelloPhoto from "../resourse/hello.png";
+
+
+
+export default function Popup() {
+
+const ref = useRef();
+
+const [isModalOpen, setModalOpen] = useState(false);
+
+useOnClickOutside(ref, () => setModalOpen(false));
+
+return (
+  <div className="body">
+    {isModalOpen ? (
+      <div className="modal__backdrop">
+        <div className="modal__container" ref={ref}>
+          👋 Hello my dear friend...
+          <img src={HelloPhoto} alt="this is hello" />
+          <button onClick={() => setModalOpen(false)}>Close</button>
+          <div className="buttonContainer"></div>
+        </div>
+      </div>
+    ) : (
+      <button onClick={() => setModalOpen(true)}>Open Modal</button>
+    )}
+  </div>
+);
+
+function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
+}
